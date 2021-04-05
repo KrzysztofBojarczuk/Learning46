7eff85bd45091ad1b4731bf030ea1910a7942ac0
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-05 09:34:05 +0200

    Add project files.

---
+++ Podatek/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Podatek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadź wartość zakupów: ");
+            double valueOfPurchases = double.Parse(Console.ReadLine());
+
+            //double taxState = valueOfPurchases * 0.04;
+            //double taxCounty = valueOfPurchases  * 0.02;
+
+
+            //double valueOfTaxes = taxState + taxCounty;
+            //double valueOfEvrything = valueOfPurchases + valueOfTaxes;
+
+            Calculate(valueOfPurchases);
+
+            //Console.WriteLine($"Wartość podatku w stnaie {taxState}");
+            //Console.WriteLine($"Wartość podatku w hrabstwie {taxCounty}");
+            //Console.WriteLine($"Wartość podatków {valueOfTaxes}");
+            //Console.WriteLine($"Wszystkie wartość {valueOfEvrything}");
+            //Console.WriteLine($"Wartość zakupów to {valueOfPurchases}");
+
+        }
+        public static void Calculate(double valueOfPurchases)
+        {
+            double taxState = valueOfPurchases * 0.04;
+            double taxCounty = valueOfPurchases * 0.02;
+
+
+            double valueOfTaxes = taxState + taxCounty;
+            double valueOfEvrything = valueOfPurchases + valueOfTaxes;
+            Console.WriteLine($"Wartość podatku w stnaie {taxState}");
+            Console.WriteLine($"Wartość podatku w hrabstwie {taxCounty}");
+            Console.WriteLine($"Wartość podatków {valueOfTaxes}");
+            Console.WriteLine($"Wszystkie wartość {valueOfEvrything}");
+            Console.WriteLine($"Wartość zakupów to {valueOfPurchases}");
+        }
+       
+    }
+}
